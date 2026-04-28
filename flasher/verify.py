9c1d1700cd6c731e6c7b7e1e28e9ac81d4def73c
phakphoum38-stack <phakphoum38@gmail.com>
2026-04-29 01:26:42 +0700

    Update verify.py

--- flasher/verify.py
+++ flasher/verify.py
@@ -2,10 +2,16 @@ import hashlib
 
 def sha(path):
     h = hashlib.sha256()
-    with open(path,"rb") as f:
-        while c:=f.read(1024*1024):
+    with open(path, "rb") as f:
+        while True:
+            c = f.read(1024 * 1024)
+            if not c:
+                break
             h.update(c)
     return h.hexdigest()
 
-def verify_flash(a,b):
-    return sha(a)==sha(b)
+def verify_flash(a, b):
+    try:
+        return sha(a) == sha(b)
+    except:
+        return False
