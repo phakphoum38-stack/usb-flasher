091aeeb796ba5992158f625a88c2950cf5407484
phakphoum38-stack <phakphoum38@gmail.com>
2026-04-29 01:28:38 +0700

    Update config.py

--- flasher/config.py
+++ flasher/config.py
@@ -2,5 +2,8 @@ import json, os
 
 def load_config():
     if not os.path.exists("config.json"):
-        json.dump({"auto_update":True},open("config.json","w"))
-    return json.load(open("config.json"))
+        with open("config.json","w") as f:
+            json.dump({"auto_update": True}, f)
+
+    with open("config.json") as f:
+        return json.load(f)
